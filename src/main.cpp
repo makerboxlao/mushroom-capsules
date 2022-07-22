c9257b42794e31ab355f91eddb1fb07e35dcce5a
KETSANA-DOUNAGPANYA <ketsanadpy@gmai.com>
2022-07-22 15:39:22 +0700

    update dht

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 #define DHTTYPE DHT22
 
+
 float humids[2];
 float temps[2];
 
@@ -11,26 +12,39 @@ float temps[2];
 // Note that older versions of this library took an optional third parameter to
 // tweak the timings for faster processors.  This parameter is no longer needed
 // as the current DHT reading algorithm adjusts itself to work on faster procs.
-DHT dht[] = {{4, DHTTYPE}, {5, DHTTYPE}};
-
-void setup() {
+DHT dht[] = {{5, DHTTYPE}, {16, DHTTYPE}};
+void setup()
+{
   Serial.begin(115200);
 
-  for (auto &sensor : dht) {
-      sensor.begin();
+
+  for (auto &sensor : dht)
+  {
+    sensor.begin();
   }
 }
 
-void loop() {
-  for (int index = 0; index < 2; index++) {
-      humids[index] = dht[index].readHumidity();
-      temps[index] = dht[index].readTemperature();
+void loop()
+{
+  
+  for (int index = 0; index < 2; index++)
+  {
+    humids[index] = dht[index].readHumidity();
+    temps[index] = dht[index].readTemperature();
   }
 
-  Serial.printf("humid 1: %f", humids[0]);
-  Serial.printf("\thumid 2: %f", humids[1]);
-  Serial.printf("\ttemp 1: %f", temps[0]);
-  Serial.printf("\ttemp 2: %f\n", temps[1]);
-
+  if (isnan(humids[0])){
+    Serial.println("\nHumindity 1 is not detected");
+  }else {
+    Serial.printf("humid 1: %f", humids[0]);
+    Serial.printf("\ttemp 1: %f", temps[0]);
+  }
+   if (isnan(humids[1])){
+    Serial.println("\nHumindity 1 is not detected");
+  }else {
+    Serial.printf("humid 2: %f", humids[1]);
+    Serial.printf("\t\ntemp 2: %f", temps[1]);
+  }
+  
   delay(2000);
 }
